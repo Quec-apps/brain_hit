1270ca3098a113cb9dfb9aac7530f47d88e382e2
Quec-apps <79459289+Quec-apps@users.noreply.github.com>
2021-04-24 14:47:42 +0530

    Add files via upload

--- content/res/variables/quiz.js
+++ content/res/variables/quiz.js
@@ -219,6 +219,503 @@ window["ans_" + Inc] = "teapot";
 window["img_t_" + Inc] = 0;
 Inc ++;
 
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "I’m green sometimes, brown other times but both cause me to frown. But just in between I’m yellow and cause me to smile! Who am I?";
+window["ans_" + Inc] = "banana";
+window["img_t_" + Inc] = 0;
+Inc ++;
 
-q_total = 36;
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "I am taken from a mine, and shut up in a wooden case, from which I am never released, and yet I am used by almost everybody";
+window["ans_" + Inc] = "pencil lead";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "When I turn around once, you can’t get in. When I turn around again. You can’t get out. What might I be?";
+window["ans_" + Inc] = "key";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "What is greater than god, more evil than devil and if you eat it you die?";
+window["ans_" + Inc] = "nothing";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "Cloud is my mother. Wind is my father. I come down, but I never go up. Who am I?";
+window["ans_" + Inc] = "rain";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "I make billions of dollars selling windows. Who am I?";
+window["ans_" + Inc] = "microsoft";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "What can you catch, but not throw?";
+window["ans_" + Inc] = "cold";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "Tear off my skin and I won't cry. But I make sure you will. Who am I?";
+window["ans_" + Inc] = "onion";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "I always wear a tuxedo and love to march. I can survive the harshest of winters. Who am I?";
+window["ans_" + Inc] = "penguin";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "I have an eye but I can’t see. I usually have to work with something in my eye. What am I?";
+window["ans_" + Inc] = "needle";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "We come out at night without being called, but we're lost in the ay without being stolen.";
+window["ans_" + Inc] = "stars";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "l am heavy forwards, but backwards I’m not. What am I?";
+window["ans_" + Inc] = "ton";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "l am gentle enough to soothe your skin, light enough to fly in the sky, strong enough to crack rocks. Who am I?";
+window["ans_" + Inc] = "water";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "I shrink smaller every time I take a bath. Who am I?";
+window["ans_" + Inc] = "soap";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "What gets bigger the more you , take away?";
+window["ans_" + Inc] = "hole";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "Six letters it contains. Take away the last and only twelve remains. What am I?";
+window["ans_" + Inc] = "dozens";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "I have a neck and no head, two arms.but no hands. I’m with you at school, with you at work. Who am I?";
+window["ans_" + Inc] = "shirt";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "l am lighter than air but a hundred people cannot lift me. Careful, I am fragile. What am I?";
+window["ans_" + Inc] = "bubbles";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "I can fly without wings and cry without eyes. Darkness follows me. Who am I?";
+window["ans_" + Inc] = "cloud";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "You can always find me in the past. I am created in the present, but the future cannot save me.";
+window["ans_" + Inc] = "history";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "What is an astronaut’s favorite key on the keyboard?";
+window["ans_" + Inc] = "space";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "You do not want me when you don’t have me. But when you have me, you don’t want to lose me";
+window["ans_" + Inc] = "lawsuit";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "l am easy to waste, but unstoppable. Who am I?";
+window["ans_" + Inc] = "time";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "I cover what’s real, hide what is true. But I bring out the courage in you and turn heads. What am I?";
+window["ans_" + Inc] = "makeup";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "What gets whiter the dirtier it gets?";
+window["ans_" + Inc] = "chalkboard";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "I'm where yesterday follows today. And tomorrow’s in the middle. What am I?";
+window["ans_" + Inc] = "dictionary";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "What is 3/7 chicken, 2/3 cat, 2/4 goat?";
+window["ans_" + Inc] = "chicago";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "Cannot be bought, cannot be sold. Even if it’s made of gold. What is it?";
+window["ans_" + Inc] = "heart";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "I have roots nobody sees. I am taller than trees. Up, up I go but I never grow. What am I?";
+window["ans_" + Inc] = "mountains";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "You throw away my outside, you cook my inside. Then you eat my outside and you throw away my inside. What am I?";
+window["ans_" + Inc] = "corn";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "You hold my tail while I fish for you. What am I?";
+window["ans_" + Inc] = "net";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "Every country has me and I begin your sentences. What am I?";
+window["ans_" + Inc] = "capital";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "So simple I am only a line. Yet I guide men all over the world";
+window["ans_" + Inc] = "compass";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "Deep, deep they go. Spreading out as they go. Never needing any air. They are sometimes as fine as hair.";
+window["ans_" + Inc] = "roots";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "Never ahead, ever behind, yet flying swiftly past. For a child I last forever, for adults I’m gone too fast";
+window["ans_" + Inc] = "youth";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "As a stone inside a tree, I'll help your words outlive thee. But if you push me as I stand, the more I move the less I am.";
+window["ans_" + Inc] = "pencil";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "I have a name written on me, but it isn’t my name. Men plant me, but I never grow. They look at me and see their future, rotting in my bloom";
+window["ans_" + Inc] = "tombstone";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "What is tight as a feather, but becomes harder to keep the longer you hold it?";
+window["ans_" + Inc] = "breath";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "A farmer has seventeen sheep. All but 10 of them die. How many sheep does he have left?";
+window["ans_" + Inc] = "10";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "l am hidden somewhere quiet, people here are purely bones, if you wish to track me down, look under the stones.";
+window["ans_" + Inc] = "graveyard";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "Walk on the living, they don’t even mumble. Walk on the dead, they mutter and grumble. What are they?";
+window["ans_" + Inc] = "leaves";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "You can drop me from the tallest building and I'll be fine, but if you drop me in water I die. What am I?";
+window["ans_" + Inc] = "paper";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "You bury me when I’m alive and dig me up when I’m dead. What am I?";
+window["ans_" + Inc] = "plant";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "I have an eye but cannot see. i am fast but i have no limbs. What am I?";
+window["ans_" + Inc] = "hurricane";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "What spends the day at the window, goes to the table for meals and hides at night?";
+window["ans_" + Inc] = "fly";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "I can be written. I can be spoken. I can be exposed. I can be broken. What am I?";
+window["ans_" + Inc] = "news";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "I am the only organ in the human body that gave itself it’s own name. What am I?";
+window["ans_" + Inc] = "brain";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "Your mother’s brother’s only brother-in-law is asleep on your couch. Who is asleep on your couch?";
+window["ans_" + Inc] = "father";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "What has been around for millions of years but is never more than a month old?";
+window["ans_" + Inc] = "moon";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "What loses it's head in the morning, and gets it back at night?";
+window["ans_" + Inc] = "pillow";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "What number do you get when you multiply all of the numbers on a telephone’s dial pad?";
+window["ans_" + Inc] = "0";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "What tastes better than it smells?";
+window["ans_" + Inc] = "tongue";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "I can only live where there is light, but I die if the light shines on me. What am I?";
+window["ans_" + Inc] = "shadow";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "What is easy to get into, but hard to get out of?";
+window["ans_" + Inc] = "trouble";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "What’s full of holes but still holds water?";
+window["ans_" + Inc] = "sponge";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "What never asks questions but is often answered?";
+window["ans_" + Inc] = "doorbell";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "I have many keys but I cannot open a single lock. What am I?";
+window["ans_" + Inc] = "keyboard";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "What can you easily break but never touch?";
+window["ans_" + Inc] = "promise";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "What three letters change a girl into a woman?";
+window["ans_" + Inc] = "age";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "What word when read from left to right is a ruler but when read right to left is a servant?";
+window["ans_" + Inc] = "dog";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "What side of a cat has the most fur?";
+window["ans_" + Inc] = "outside";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "Say my name and i am no more";
+window["ans_" + Inc] = "silence";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "What english word sounds the same even after you take away four of its five letters?";
+window["ans_" + Inc] = "queue";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "What goes up when rain comes down?";
+window["ans_" + Inc] = "umbrellas";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "I travel all over the world, but always stay in my corner. What am I?";
+window["ans_" + Inc] = "stamp";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "What has six faces, twenty one eyes but cannot see?";
+window["ans_" + Inc] = "dice";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "You go at red, but stop at green. What am I?";
+window["ans_" + Inc] = "watermelon";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "You are my brother, but I am not your brother. Who am I?";
+window["ans_" + Inc] = "sister";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "I am not alive, but I grow; I don’t have lungs, but I need air; Idon’t have a mouth, but water kills me. What am I?";
+window["ans_" + Inc] = "fire";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "Poor people have it. Rich people need it. If you eat it you die. What is it?";
+window["ans_" + Inc] = "nothing";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "I am a seed with three letters in my name. Take away the last two and I still sound the same. What am I?";
+window["ans_" + Inc] = "pea";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "I run in and out of town all day and night and yet I never get tired? What am I?";
+window["ans_" + Inc] = "road";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "What does no man want, yet no man want to lose?";
+window["ans_" + Inc] = "job";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "If an electric train is travelling south, which way is the smoke going?";
+window["ans_" + Inc] = "no smoke";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "What kind of cup doesn’t hold water?";
+window["ans_" + Inc] = "cupcake";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "You will throw me away when you want to use me. You will take me in when you don’t want to use me. What am I?";
+window["ans_" + Inc] = "anchor";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "It gets beaten, and whipped, but never cries? What is it?";
+window["ans_" + Inc] = "egg";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "It is higher without the head, than with it. What is it?";
+window["ans_" + Inc] = "pillow";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "What has a neck but no head?";
+window["ans_" + Inc] = "bottle";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "What starts with a p, ends with an e and has thousands of letters?";
+window["ans_" + Inc] = "post office";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "What ship has two mates, but no captain?";
+window["ans_" + Inc] = "relationship";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "Before mount everest was discovered, what was the highest mountain on earth?";
+window["ans_" + Inc] = "mount everest";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+window["t_" + Inc] = svg;
+window["q_" + Inc] = "I make you, I’m always in you; if I surround you, I can kill you. What am I?";
+window["ans_" + Inc] = "water";
+window["img_t_" + Inc] = 0;
+Inc ++;
+
+q_total = 119;
 isLoaded = true;
